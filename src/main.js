7a7ae47064e895654f6e34dbe505305b8f1e7faa
Automne Catin <shamo@hotmail.fr>
2020-05-19 13:18:39 +0200

    test deploy

--- src/main.js
+++ src/main.js
@@ -1,12 +1,40 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import firebase from "firebase/app";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+var firebaseConfig = {
+    apiKey: "AIzaSyBRqreWtTEw1kT7iyRpTqkrsALP8ncmIhE",
+    authDomain: "boutiqueauthentification.firebaseapp.com",
+    databaseURL: "https://boutiqueauthentification.firebaseio.com",
+    projectId: "boutiqueauthentification",
+    storageBucket: "boutiqueauthentification.appspot.com",
+    messagingSenderId: "859820151366",
+    appId: "1:859820151366:web:32ef100da920e57fe1b047"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+let app;
+
+firebase.auth().onAuthStateChanged(user => {
+  console.log("user", user);
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
+
+// app = new Vue({
+//   router,
+//   store,
+//   render: h => h(App)
+// }).$mount("#app");
